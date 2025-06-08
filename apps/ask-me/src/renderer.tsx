07678d7e237adcd89874bbbecc3a5d217bf1ee23
Aotokitsuruya <contact@aotoki.me>
2025-06-08 11:29:35 +0800

    feat: add page title to renderer component

--- apps/ask-me/src/renderer.tsx
+++ apps/ask-me/src/renderer.tsx
@@ -8,6 +8,7 @@ export const renderer = jsxRenderer(({ children }) => {
         <ViteClient />
         <Script src="/src/client.tsx" />
         <Link href="/src/style.css" rel="stylesheet" />
+        <title>Ask Aotokitsuruya</title>
       </head>
       <div id="app">{children}</div>
     </html>
